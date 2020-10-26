3e7243e0793ebad943d0bd00766bb3a07bf319c8
Lawrence Wallace <lwallac21@gmail.com>
2020-10-26 10:35:30 -0400

    Done!

--- index.js
+++ index.js
@@ -4,66 +4,71 @@ const path = require("path")
 const generateMarkdown = require("./utils/generateMarkdown")
 // array of questions for user
 const questions = [
-{
-    type: "input",
-    name: "github",
-    message: "What is your github username?"
-},
-{
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?"
-},
-{
-   type: "input", 
-   name: "installation",
-   message: "Please provide installation instructions."
-},
-{
-    type: "input",
-    name: "usage",
-    message: "Share usage instructions."
-},
-{
-    type: "list",
-    name: "license",
-    choices: [
-        "MIT license",
-        "Apache license",
-        "GPL license",
-        "Public Domain"
-    ]
-},
-{
-    type: "input",
-    name: "author",
-    message: "Who authored this project?"
-},
-{
-    type: "input",
-    name: "photo",
-    message: "Please provide the path to your profile photo."
-},
-{
-    type: "input",
-    name: "email",
-    message: "Enter your email address."
-},
-{
-    type: "input",
-    name: "contributors",
-    message: "Did anyone else contribute to your project? Please provide their github profiles, separated by commas."
-},
-{
-    type: "input",
-    name: "test",
-    message: "How can a user run tests on the command line?"
-},
-{
-    type: "input",
-    name: "contribute",
-    message: "If a user needs to, how should they to contribute to this repo."
-}
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github username?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a description of your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide installation instructions."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Share usage instructions."
+    },
+    {
+        type: "list",
+        name: "license",
+        choices: [
+            "MIT license",
+            "Apache license",
+            "GPL license",
+            "Public Domain"
+        ]
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "Who authored this project?"
+    },
+    {
+        type: "input",
+        name: "photo",
+        message: "Please provide the path to your profile photo."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address."
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Did anyone else contribute to your project? Please provide their github profiles, separated by commas."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How can a user run tests on the command line?"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "If a user needs to, how should they to contribute to this repo."
+    }
 ];
 
 // function to write README file
@@ -75,13 +80,8 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then(response => {
         console.log("generate readme...");
-        writeToFile("README.MD", generateMarkdown({...response})); 
-    })}
+        writeToFile("README.MD", generateMarkdown({ ...response }));
+    })
+}
 
-     // .then function
-
-    // call the function writeToFile using the data that we pass (ie the inquirer responses)
-
-
-// function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,37 @@
-// function passing in liscense as an arg
-// validate lisense exists
-// return ![GitHub license](https://img.shields.io/badge/license- ROUTE HERE -blue.svg)
-
-
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `#${data.title}
-##Table of Contents:
+  return `# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents:
 -Installation
 -Usage
 -Credits
 -License
 
-##Installation Instructions
+## Installation Instructions
 ${data.installation}
 
-##Usage
+## Usage
 ${data.usage}
 
-###Running tests
+### Running tests
 ${data.test}
 
-###Contributing to the repo
+### Contributing to the repo
 
-##Credits
+## Credits
 Created by ${data.author} @ ${data.github}
 !(${data.photo})
 
 
-###Contributors
-${data.contributors[0]} @ ${data.contributors[1]}
+### Contributors
+${data.contributors}
 
-##License
+## License
 This project uses the ${data.license}.
-
-
 `;
 }
 
